8323390621ca761c61ca0741a43c2c85108c34c3
Y.Shinohara <shinohara@2ndwave.jp>
2021-05-17 16:36:31 +0900

    Initial Commit

---
+++ KeyManage/KeyManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyManageApp.swift
+//  KeyManage
+//
+//  Created by Y.Shinohara on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct KeyManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
